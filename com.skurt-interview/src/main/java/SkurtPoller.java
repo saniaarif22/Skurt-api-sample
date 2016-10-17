45c8e593d45b02cbf68a0191d744dee5bd794429
Sania Arif <sania.arif@columbia.edu>
2016-10-17 02:50:07 -0400

    main sample file

---
+++ com.skurt-interview/src/main/java/SkurtPoller.java
@@ -0,0 +1,32 @@
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import model.Vehicle;
+import service.ParserService;
+import service.PollingService;
+import service.SkurtAPIService;
+
+public class SkurtPoller {
+	//TODO: Check for nulls 
+	public static void main(String args[]) {
+		Queue<Vehicle> vehicleQueue = new LinkedList<Vehicle>();
+		SkurtAPIService skurtAPIservice = new SkurtAPIService();
+		ParserService parserService = new ParserService();
+		
+		/* Polling data from SkurtAPIService,
+		 * Parsing the JSON using ParserService
+		 * and adding to a queue */
+		for(int i = 1; i<= 10; i++) {
+			InputStream is = skurtAPIservice.sendRequest(i);
+			Vehicle vehicle = parserService.parse(i, is);
+			vehicleQueue.add(vehicle);
+		}
+		System.out.println("Vehicle queue size in main: " + vehicleQueue.size());
+		
+		PollingService pollingService = new PollingService(vehicleQueue);
+		System.out.println("Vehicle queue size of pollingService in main: " + pollingService.vehicleQueue.size());
+		pollingService.monitor();
+	}
+
+}
