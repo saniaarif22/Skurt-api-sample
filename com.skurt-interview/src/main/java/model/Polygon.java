9e3f7378e897e37fc51370ff71ef275353f7965b
Sania Arif <sania.arif@columbia.edu>
2016-10-23 19:15:48 -0400

    Polygon model

---
+++ com.skurt-interview/src/main/java/model/Polygon.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Polygon {
+	/* List of vertices of polygon */
+	ArrayList<Coordinates> vertices;
+	
+	public Polygon (ArrayList<Coordinates> vertices) {
+		this.vertices = vertices;
+	}
+}
