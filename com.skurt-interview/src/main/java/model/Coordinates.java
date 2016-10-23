3c0ca834ab37c07acc018107bdc8f1854849ae1c
Sania Arif <sania.arif@columbia.edu>
2016-10-23 19:15:28 -0400

    Coordinates class for car

---
+++ com.skurt-interview/src/main/java/model/Coordinates.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Coordinates {
+	double x, y;
+
+	public Coordinates (double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+}
