8ace44f825adc0f92c47a111b52dd5cdc8d69da6
Sania Arif <sania.arif@columbia.edu>
2016-10-25 19:05:57 -0400

    Standard java SMTP client

---
+++ com.skurt-interview/src/main/java/service/SMTPClient.java
@@ -0,0 +1,54 @@
+package service;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import model.Car;
+import model.Vehicle;
+
+public class SMTPClient {
+	/* Populate fields as required */
+	private static final String TO_ADDRESS = "";
+	private static String FROM = "";
+	private static String PASSWORD = "";
+
+	/* Standard java mail code stub to send an e-mail */
+	public boolean sendEmail(Vehicle vehicle) {
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtp.host", HOST);
+		props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.user", FROM);
+        props.put("mail.smtp.password", PASSWORD);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        
+		Session session = Session.getDefaultInstance(props);
+		
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(FROM));
+			message.addRecipient(Message.RecipientType.TO,
+					new InternetAddress(TO_ADDRESS));
+			message.setSubject("Status update for Car#" + vehicle.getVehicleId());
+			message.setContent("<h3>The following car is out of range <br>" + 
+								vehicle.getVehicleId() +  "</h3>", "text/html");
+
+			Transport transport = session.getTransport("smtp");
+            transport.connect(HOST, FROM, PASSWORD);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+            /* E-mail alert sent */
+            return true;
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+			return false; /* No e-mail alert sent */
+			}
+	}
+ }
